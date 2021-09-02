be0d944deb2decf37bf7c91cde54c99b3a2e1b4c
ZANGZANGS 49123158+ZANGZANGS@users.noreply.github.com <kimjangh94@gmail.com>
2021-09-02 10:59:16 +0900

    Initial commit

---
+++ src/gof/dp/singleton/ConnectionPool.java
@@ -0,0 +1,18 @@
+package gof.dp.singleton;
+
+public class ConnectionPool {
+
+    private static ConnectionPool instance = new ConnectionPool();
+
+    //생성자의 접근제어자를 private로
+    private ConnectionPool(){}
+
+    //외부에서 접근 가능한 static 메서드 생성
+    public static ConnectionPool getInstance(){
+        if(null ==instance){
+            instance = new ConnectionPool();
+        }
+
+        return instance;
+    }
+}
